de961b239d25515d658d7338c651a0636b4ba03c
NinaChavezz <chavez.carolinaok@gmail.com>
2023-05-09 15:51:39 -0300

    railwaydb

---
+++ src/main/java/com/portfolio/acc/Security/Util/Util.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.acc.Security.Util;
+
+import com.portfolio.acc.Security.Entity.Rol;
+import com.portfolio.acc.Security.Enums.RolNombre;
+import com.portfolio.acc.Security.Service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/*
+@Component
+public class Util implements CommandLineRunner {
+
+    @Autowired
+    RolService rolService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        rolService.save(rolAdmin);
+        rolService.save(rolUser);
+    }
+
+}
+*/
